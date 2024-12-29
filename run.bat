5ed3d7b41dcf172207e4390c4d98950934aef98d
aburahamu <atsushi.aburahamu@gmail.com>
2024-12-29 14:38:29 +0900

    modified run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 @echo off
 chcp 65001
 
-if not defined PYTHON (set PYTHON=python)
-if not defined VENV_DIR (set "VENV_DIR=%~dp0%venv")
+if not defined PYTHON set PYTHON=python
+if not defined VENV_DIR set "VENV_DIR=%~dp0venv"
 
 set ERROR_REPORTING=FALSE
 
@@ -11,28 +11,39 @@ if %ERRORLEVEL% == 0 goto :activate_venv
 
 echo.
 echo [CREATE VENV]
-for /f "delims=" %%i in ('CALL %PYTHON% -c "import sys; print(sys.executable)"') do set PYTHON_FULLNAME="%%i"
+for /f "delims=" %%i in ('%PYTHON% -c "import sys; print(sys.executable)"') do set PYTHON_FULLNAME=%%i
 echo Creating venv in directory %VENV_DIR% using python %PYTHON_FULLNAME%
 %PYTHON_FULLNAME% -m venv "%VENV_DIR%"
-if %ERRORLEVEL% == 0 goto :activate_venv
-echo Unable to create venv in directory "%VENV_DIR%"
+if %ERRORLEVEL% neq 0 (
+    echo Unable to create venv in directory "%VENV_DIR%"
+    exit /b
+)
 
 :activate_venv
 echo.
 echo [ACTIVATE VENV]
-set PYTHON="%VENV_DIR%\Scripts\Python.exe"
+set PYTHON=%VENV_DIR%\Scripts\Python.exe
 echo venv %PYTHON%
 
 echo.
 echo [UPGRADE PIP]
-python -m pip install --upgrade pip
+%PYTHON% -m pip install --upgrade pip
 
 echo.
 echo [INSTALL REQUIREMENTS]
-pip install --upgrade -r requirements.txt
+%PYTHON% -m pip install --upgrade -r requirements.txt
+if %ERRORLEVEL% neq 0 (
+    echo Unable to install requirements.
+    exit /b
+)
 
 echo.
-echo [LAUCH APP]
+echo [LAUNCH APP]
 %PYTHON% app.py
+if %ERRORLEVEL% neq 0 (
+    echo Application failed to launch.
+    exit /b
+)
+
 pause
 exit /b
